a4c655c1e5984495d0aab3b15cfa7f61537f6c00
Marwan Rahim <marwan820@Marwan>
2023-09-18 01:48:56 -0500

    installed react router

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header'
 
-
 function App() {
   const [count, setCount] = useState(0)
 
@@ -18,7 +17,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      {Header}
+      < Header/>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
