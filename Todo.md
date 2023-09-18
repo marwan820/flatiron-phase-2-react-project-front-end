d88be788d428cef194694e85e01ee279db8ea5b6
Marwan Rahim <marwan820@Marwan>
2023-09-18 17:28:38 -0500

    deleted default vite text content, inserted header

---
+++ Todo.md
@@ -0,0 +1,4 @@
+# To-do list
+
+[] create a list of cat products
+[] list of cat products should
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
 export default function Header() {
   return (
     <div>
-      <header>
-        <h1> React Project</h1>
+      <header className="header">
+        <h1> Cats R'Us</h1>
       </header>
     </div>
   );
